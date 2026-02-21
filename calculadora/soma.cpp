2ee8d7fabe90e0cbc66b5a8381541af91005179a
IgorMaiaSales <51336504+IgorMaiaSales@users.noreply.github.com>
2026-02-21 14:45:52 +0000

    Aula 1

---
+++ calculadora/soma.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int a, b;
+
+    cout << "Quais os números que você quer somar? ";
+    cin >> a >> b;
+    
+    cout << "A soma dos números entregues é: " << (a + b) << "\n";
+    
+    return 0;
+}
